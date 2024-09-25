6b5ddba0ef02c16e55d464214e62fb408d3548b4
meydhimeydhi@gmail.com <meydhimeydhi@gmail.com>
2024-09-25 22:27:52 +0800

    feat : add

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const showText = ref(false)
+</script>
+
+<template>
+  <main>
+    <Transition name="fade">
+      <h1 v-if="showText">Medev</h1>
+    </Transition>
+    <button @click="showText = !showText">Toggle</button>
+  </main>
+</template>
+<style scoped>
+.fade-enter-from{
+  opacity: 0;
+}
+
+.fade-enter-to{
+  opacity: 1;
+}
+
+.fade-enter-active{
+  transition: all 1s ease;
+}
+</style>
